bfad1aa682b2ab854b1bca457b93acc0e9e3cb82
jenkassim <jenkassim@gmail.com>
2017-12-06 00:36:21 +0800

    wk4 Assignment

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,29 +1,61 @@
 def create_bookstore(name):
-    pass
+    store = {}
+    store["name"] = name
+    store["authors"] = {}
+    store["authors_ids"] = 0
+    store["books"] = {}
+    store["books_ids"] = 0
+
+    return store
+
+def print_dict(mydict):
+    for key, val in mydict.items():
+        print(str.format("[{0}] : {1}", key, val))
 
 def add_author(bookstore, name, nationality):
-    pass
+    author = {}
+    bookstore["authors_ids"] = bookstore["authors_ids"] + 1
+    author["name"] = name
+    author["nationality"] = nationality
+    author["id"] = bookstore["authors_ids"]
 
+    bookstore["authors"][author["id"]] = author
+    return author
 
 def get_author_by_name(bookstore, name):
-    pass
-
+    for author_id, authors_dict in bookstore["authors"].items():
+        if authors_dict['name'] == name:
+            return authors_dict
 
 def get_author_by_id(bookstore, author_id):
-    pass
-
+    return bookstore["authors"][author_id]
 
 def add_book(bookstore, title, isbn, author_id):
-    pass
+    book = {}
+    bookstore["books_ids"] = bookstore["books_ids"] + 1
+    book["title"] = title
+    book["isbn"] = isbn
+    book["author_id"] = author_id
+    book["id"] = bookstore["books_ids"]
 
+    bookstore["books"][book["id"]] = book
+    return book
 
 def get_book_by_title(bookstore, title):
-    pass
-
+    for book_id, book_dict in bookstore["books"].items():
+        if book_dict['title'] == title:
+            return book_dict
 
 def get_book_by_id(bookstore, book_id):
-    pass
-
+    return bookstore["books"][book_id]
 
 def get_books_by_author(bookstore, author_id):
-    pass
+    book_list = []
+    print_dict(bookstore)
+    for book_id, book_dict in bookstore["books"].items():
+        print_dict(book_dict)
+        if book_dict["author_id"] == author_id:
+
+            book_list.append(book_dict)
+
+    return book_list
